c44d702c81ca1a7cda8cd9ac4f266697521032c0
Andrei Joghiu <raiden007@gmail.com>
2015-04-28 16:28:35 +0300

    Started working on the interface

---
+++ Deck Tracker/AddDeck.swift
@@ -0,0 +1,47 @@
+//
+//  AddDeck.swift
+//  Deck Tracker
+//
+//  Created by Andrei Joghiu on 28/4/15.
+//  Copyright (c) 2015 Andrei Joghiu. All rights reserved.
+//
+
+import UIKit
+
+class AddDeck: UIViewController {
+
+    @IBOutlet var cancelButton: UIBarButtonItem!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    @IBAction func cancelButtonPressed(sender: UIBarButtonItem) {
+        self.dismissViewControllerAnimated(true, completion: {})
+        println("Add Deck dismissed")
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Deck Tracker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Deck Tracker
+//
+//  Created by Andrei Joghiu on 28/4/15.
+//  Copyright (c) 2015 Andrei Joghiu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
