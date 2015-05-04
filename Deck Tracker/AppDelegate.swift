98eb404aa3f8919f107e59d50c021588c6e90128
Andrei Joghiu <raiden007@gmail.com>
2015-05-04 15:08:16 +0300

    Added some functionality to create Matches objects

--- Deck Tracker/AppDelegate.swift
+++ Deck Tracker/AppDelegate.swift
@@ -15,7 +15,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        // Override point for customization after application launch
+        
+        var matches:[Match] = []
+        
+        var match = Match()
+        match.opponentDeck = "Opponent Deck"
+        match.playerDeck = "Player Deck"
+        match.coin = true
+//        match.win = false
+        
+        matches.append(match)
+        
+        
+        println(match.opponentDeck)
+        println(match.playerDeck)
+//        println(match.win)
+        println(match.coin)
+        
+
         return true
     }
 
---
+++ Deck Tracker/Match.swift
@@ -0,0 +1,35 @@
+//
+//  Match.swift
+//  Deck Tracker
+//
+//  Created by Andrei Joghiu on 4/5/15.
+//  Copyright (c) 2015 Andrei Joghiu. All rights reserved.
+//
+
+import Foundation
+
+class Match {
+    
+    var playerDeck = "null"
+    var opponentDeck = "null"
+    var coin:Bool = false
+//    var win:Bool
+
+    
+    
+    func getPlayerDeck() -> String {
+        return playerDeck
+
+    }
+    
+    func getOpponentDeck() -> String {
+        return opponentDeck
+    }
+    
+
+
+    
+    
+    
+    
+}
