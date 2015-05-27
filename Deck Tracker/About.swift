c8bbbe4b504303d9218c32d4b140d016756d5456
Andrei Joghiu <raiden007@gmail.com>
2015-05-27 12:18:23 +0300

    Added About Page

---
+++ Deck Tracker/About.swift
@@ -0,0 +1,35 @@
+//
+//  About.swift
+//  Deck Tracker
+//
+//  Created by Andrei Joghiu on 27/5/15.
+//  Copyright (c) 2015 Andrei Joghiu. All rights reserved.
+//
+
+import UIKit
+
+class About: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
