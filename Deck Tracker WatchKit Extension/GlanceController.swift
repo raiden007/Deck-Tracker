d7383e61f29921e445675290e74496b320f4d640
Andrei Joghiu <raiden007@gmail.com>
2015-05-29 17:17:44 +0300

    Added Watch interface

---
+++ Deck Tracker WatchKit Extension/GlanceController.swift
@@ -0,0 +1,31 @@
+//
+//  GlanceController.swift
+//  Deck Tracker WatchKit Extension
+//
+//  Created by Andrei Joghiu on 29/5/15.
+//  Copyright (c) 2015 Andrei Joghiu. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class GlanceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
