baf2aeb4944616f7d0d96972dbea5fd42362aafa
Andrei Joghiu <raiden007@gmail.com>
2015-06-04 10:30:39 +0300

    Added logic to select deck button

--- Deck Tracker WatchKit Extension/DeckTrackerWatch.swift
+++ Deck Tracker WatchKit Extension/DeckTrackerWatch.swift
@@ -72,6 +72,7 @@ class DeckTrackerWatch: WKInterfaceController {
             var selectedDeckClass = defaults.stringForKey("Selected Deck Class")!
             println(selectedDeckClass)
             selectDeckButton.setTitle(selectedDeckName)
+            colorCell(selectedDeckClass, button: selectDeckButton, opponent: false, deckName: selectedDeckName)
         }
 
     }
@@ -79,90 +80,124 @@ class DeckTrackerWatch: WKInterfaceController {
     func setOpponentClassButton() {
         if let opponentClass = NSUserDefaults.standardUserDefaults().stringForKey("Watch Opponent Class") {
             //selectOpponentButton.setTitle("Opponent: " + String(opponentClass))
-            if opponentClass == "Warrior" {
-                selectOpponentButton.setBackgroundColor(UIColorFromRGB(0xCC0000))
-                
-                var boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
-                var greenColor = UIColor.greenColor()
-                var attributeDictionary = [NSFontAttributeName: boldFont, NSForegroundColorAttributeName: greenColor]
-                var attibuteString = NSAttributedString(string: "Versus: Warrior", attributes: attributeDictionary)
-                selectOpponentButton.setAttributedTitle(attibuteString)
-                
-            } else if opponentClass == "Paladin" {
-                selectOpponentButton.setBackgroundColor(UIColorFromRGB(0xCCC333))
-                
-                var boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
-                var greenColor = UIColor.greenColor()
-                var attributeDictionary = [NSFontAttributeName: boldFont, NSForegroundColorAttributeName: greenColor]
-                var attibuteString = NSAttributedString(string: "Versus: Paladin", attributes: attributeDictionary)
-                selectOpponentButton.setAttributedTitle(attibuteString)
-                
-            } else if opponentClass == "Shaman" {
-                selectOpponentButton.setBackgroundColor(UIColorFromRGB(0x3366CC))
-                
-                var boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
-                var greenColor = UIColor.greenColor()
-                var attributeDictionary = [NSFontAttributeName: boldFont, NSForegroundColorAttributeName: greenColor]
-                var attibuteString = NSAttributedString(string: "Versus: Shaman", attributes: attributeDictionary)
-                selectOpponentButton.setAttributedTitle(attibuteString)
-                
-            } else if opponentClass == "Hunter" {
-                selectOpponentButton.setBackgroundColor(UIColorFromRGB(0x339933))
-                
-                var boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
-                var greenColor = UIColor.greenColor()
-                var attributeDictionary = [NSFontAttributeName: boldFont, NSForegroundColorAttributeName: greenColor]
-                var attibuteString = NSAttributedString(string: "Versus: Hunter", attributes: attributeDictionary)
-                selectOpponentButton.setAttributedTitle(attibuteString)
-                
-            } else if opponentClass == "Druid" {
-                selectOpponentButton.setBackgroundColor(UIColorFromRGB(0x990000))
-                
-                var boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
-                var greenColor = UIColor.greenColor()
-                var attributeDictionary = [NSFontAttributeName: boldFont, NSForegroundColorAttributeName: greenColor]
-                var attibuteString = NSAttributedString(string: "Versus: Druid", attributes: attributeDictionary)
-                selectOpponentButton.setAttributedTitle(attibuteString)
-                
-            } else if opponentClass == "Rogue" {
-                selectOpponentButton.setBackgroundColor(UIColorFromRGB(0x666666))
-                
-                var boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
-                var greenColor = UIColor.greenColor()
-                var attributeDictionary = [NSFontAttributeName: boldFont, NSForegroundColorAttributeName: greenColor]
-                var attibuteString = NSAttributedString(string: "Versus: Rogue", attributes: attributeDictionary)
-                selectOpponentButton.setAttributedTitle(attibuteString)
-                
-            } else if opponentClass == "Warlock" {
-                selectOpponentButton.setBackgroundColor(UIColorFromRGB(0x9900CC))
-                
-                var boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
-                var greenColor = UIColor.greenColor()
-                var attributeDictionary = [NSFontAttributeName: boldFont, NSForegroundColorAttributeName: greenColor]
-                var attibuteString = NSAttributedString(string: "Versus: Warlock", attributes: attributeDictionary)
-                selectOpponentButton.setAttributedTitle(attibuteString)
-                
-            } else if opponentClass == "Mage" {
-                selectOpponentButton.setBackgroundColor(UIColorFromRGB(0x009999))
-                
-                var boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
-                var greenColor = UIColor.greenColor()
-                var attributeDictionary = [NSFontAttributeName: boldFont, NSForegroundColorAttributeName: greenColor]
-                var attibuteString = NSAttributedString(string: "Versus: Mage", attributes: attributeDictionary)
-                selectOpponentButton.setAttributedTitle(attibuteString)
-                
-            } else if opponentClass == "Priest" {
-                selectOpponentButton.setBackgroundColor(UIColorFromRGB(0x999999))
-                
-                var boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
-                var greenColor = UIColor.greenColor()
-                var attributeDictionary = [NSFontAttributeName: boldFont, NSForegroundColorAttributeName: greenColor]
-                var attibuteString = NSAttributedString(string: "Versus: Priest", attributes: attributeDictionary)
-                selectOpponentButton.setAttributedTitle(attibuteString)
-            }
+            colorCell(opponentClass, button: selectOpponentButton, opponent: true, deckName: "")
             
         } else {
             selectOpponentButton.setTitle("Select Opponent")
+        }
+    }
+    
+    // Colors the cell according to the Class specified
+    func colorCell (classToBeColored:String, button:WKInterfaceButton, opponent:Bool, deckName:String) {
+        
+        if classToBeColored == "Warrior" {
+            button.setBackgroundColor(UIColorFromRGB(0xCC0000))
+            var boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
+            var greenColor = UIColor.greenColor()
+            var attributeDictionary = [NSFontAttributeName: boldFont, NSForegroundColorAttributeName: greenColor]
+            if opponent == true {
+               var attibuteString = NSAttributedString(string: "Versus: Warrior", attributes: attributeDictionary)
+                button.setAttributedTitle(attibuteString)
+            } else {
+                var attibuteString = NSAttributedString(string: "Deck: " + deckName, attributes: attributeDictionary)
+                button.setAttributedTitle(attibuteString)
+            }
+        } else if classToBeColored == "Paladin" {
+            button.setBackgroundColor(UIColorFromRGB(0xCCC333))
+            var boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
+            var greenColor = UIColor.greenColor()
+            var attributeDictionary = [NSFontAttributeName: boldFont, NSForegroundColorAttributeName: greenColor]
+            if opponent == true {
+                var attibuteString = NSAttributedString(string: "Versus: Paladin", attributes: attributeDictionary)
+                button.setAttributedTitle(attibuteString)
+            } else {
+                var attibuteString = NSAttributedString(string: "Deck: " + deckName, attributes: attributeDictionary)
+                button.setAttributedTitle(attibuteString)
+            }
+        } else if classToBeColored == "Shaman" {
+            button.setBackgroundColor(UIColorFromRGB(0x3366CC))
+            var boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
+            var greenColor = UIColor.greenColor()
+            var attributeDictionary = [NSFontAttributeName: boldFont, NSForegroundColorAttributeName: greenColor]
+            if opponent == true {
+                var attibuteString = NSAttributedString(string: "Versus: Shaman", attributes: attributeDictionary)
+                button.setAttributedTitle(attibuteString)
+            } else {
+                var attibuteString = NSAttributedString(string: "Deck: " + deckName, attributes: attributeDictionary)
+                button.setAttributedTitle(attibuteString)
+            }
+        } else if classToBeColored == "Hunter" {
+            button.setBackgroundColor(UIColorFromRGB(0x339933))
+            var boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
+            var greenColor = UIColor.greenColor()
+            var attributeDictionary = [NSFontAttributeName: boldFont, NSForegroundColorAttributeName: greenColor]
+            if opponent == true {
+                var attibuteString = NSAttributedString(string: "Versus: Hunter", attributes: attributeDictionary)
+                button.setAttributedTitle(attibuteString)
+            } else {
+                var attibuteString = NSAttributedString(string: "Deck: " + deckName, attributes: attributeDictionary)
+                button.setAttributedTitle(attibuteString)
+            }
+        } else if classToBeColored == "Druid" {
+            button.setBackgroundColor(UIColorFromRGB(0x990000))
+            var boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
+            var greenColor = UIColor.greenColor()
+            var attributeDictionary = [NSFontAttributeName: boldFont, NSForegroundColorAttributeName: greenColor]
+            if opponent == true {
+                var attibuteString = NSAttributedString(string: "Versus: Druid", attributes: attributeDictionary)
+                button.setAttributedTitle(attibuteString)
+            } else {
+                var attibuteString = NSAttributedString(string: "Deck: " + deckName, attributes: attributeDictionary)
+                button.setAttributedTitle(attibuteString)
+            }
+        } else if classToBeColored == "Rogue" {
+            button.setBackgroundColor(UIColorFromRGB(0x666666))
+            var boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
+            var greenColor = UIColor.greenColor()
+            var attributeDictionary = [NSFontAttributeName: boldFont, NSForegroundColorAttributeName: greenColor]
+            if opponent == true {
+                var attibuteString = NSAttributedString(string: "Versus: Rogue", attributes: attributeDictionary)
+                button.setAttributedTitle(attibuteString)
+            } else {
+                var attibuteString = NSAttributedString(string: "Deck: " + deckName, attributes: attributeDictionary)
+                button.setAttributedTitle(attibuteString)
+            }
+        } else if classToBeColored == "Warlock" {
+            button.setBackgroundColor(UIColorFromRGB(0x9900CC))
+            var boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
+            var greenColor = UIColor.greenColor()
+            var attributeDictionary = [NSFontAttributeName: boldFont, NSForegroundColorAttributeName: greenColor]
+            if opponent == true {
+                var attibuteString = NSAttributedString(string: "Versus: Warlock", attributes: attributeDictionary)
+                button.setAttributedTitle(attibuteString)
+            } else {
+                var attibuteString = NSAttributedString(string: "Deck: " + deckName, attributes: attributeDictionary)
+                button.setAttributedTitle(attibuteString)
+            }
+        } else if classToBeColored == "Mage" {
+            button.setBackgroundColor(UIColorFromRGB(0x009999))
+            var boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
+            var greenColor = UIColor.greenColor()
+            var attributeDictionary = [NSFontAttributeName: boldFont, NSForegroundColorAttributeName: greenColor]
+            if opponent == true {
+                var attibuteString = NSAttributedString(string: "Versus: Mage", attributes: attributeDictionary)
+                button.setAttributedTitle(attibuteString)
+            } else {
+                var attibuteString = NSAttributedString(string: "Deck: " + deckName, attributes: attributeDictionary)
+                button.setAttributedTitle(attibuteString)
+            }
+        } else if classToBeColored == "Priest" {
+            button.setBackgroundColor(UIColorFromRGB(0x999999))
+            var boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
+            var greenColor = UIColor.greenColor()
+            var attributeDictionary = [NSFontAttributeName: boldFont, NSForegroundColorAttributeName: greenColor]
+            if opponent == true {
+                var attibuteString = NSAttributedString(string: "Versus: Priest", attributes: attributeDictionary)
+                button.setAttributedTitle(attibuteString)
+            } else {
+                var attibuteString = NSAttributedString(string: "Deck: " + deckName, attributes: attributeDictionary)
+                button.setAttributedTitle(attibuteString)
+            }
         }
     }
 
