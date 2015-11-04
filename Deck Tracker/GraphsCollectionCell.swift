fc281a5ce6c2f513c4f90887551365b502f9838f
Andrei Joghiu <raiden007@gmail.com>
2015-11-04 15:48:53 +0200

    Added some graphs, needs more refining

--- Deck Tracker/GraphsCollectionCell.swift
+++ Deck Tracker/GraphsCollectionCell.swift
@@ -14,7 +14,36 @@ class GraphsCollectionCell: UICollectionViewCell {
     @IBOutlet weak var graphsLabel: UILabel!
     @IBOutlet weak var winInfoLabel: UILabel!
     
-    @IBOutlet weak var testLabel: UILabel!
     
+    override func drawRect(rect: CGRect) {
+        let path = UIBezierPath(ovalInRect: rect)
+        let graphs = graphsLabel.text as String!
+        var winRateString = ""
+        var index1 = graphs.endIndex.advancedBy(-2)
+        
+        var substring1 = graphs.substringToIndex(index1)
+        
+        if graphs == "No Data" {
+            winRateString = ""
+        } else {
+            winRateString = substring1
+        }
+        
+        print(winRateString)
+        var winRate:Int = -1
+        if winRateString == "" {
+            winRate = 0
+        } else {
+            winRate = Int(winRateString) as Int!
+        }
+        
+        if winRate > 50 {
+            UIColor.greenColor().setFill()
+        } else {
+            UIColor.redColor().setFill()
+        }
+        //UIColor.greenColor().setFill()
+        path.fill()
+    }
     
 }
